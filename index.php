2ff6736f67faae38723b10cfb6870970004e1c2a
Rafael <rmartin65@gmail.com>
2024-05-09 13:54:14 +0100

    new script

--- index.php
+++ index.php
@@ -143,6 +143,27 @@
                 echo "La suma de los primeros 1000 números enteros es: $suma";
                 ?>
 
+<?php
+$suma_pares = 0;
+$producto_impares = 1;
+$pares_contados = 0;
+$impares_contados = 0;
+
+for ($i = 1; $pares_contados < 20 || $impares_contados < 20; $i++) {
+    if ($i % 2 == 0 && $pares_contados < 20) {
+        $suma_pares += $i;
+        $pares_contados++;
+    } elseif ($i % 2 != 0 && $impares_contados < 20) {
+        $producto_impares *= $i;
+        $impares_contados++;
+    }
+}
+
+echo "La suma de los primeros 20 números pares es: $suma_pares<br>";
+echo "El producto de los primeros 20 números impares es: $producto_impares";
+?>
+
+
                 <!-- Pager
                 <br>
                 <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase"
