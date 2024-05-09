12a24144bb6384e3317474aa44f9fa63682b8451
Rafael <rmartin65@gmail.com>
2024-05-09 09:32:20 +0100

    comment inicio on menu

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.php">Inicio</a></li>
+                    <!--<li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.php">Inicio</a></li>-->
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="#">Herramientas</a></li>
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="#">Curiosidades</a></li>
                     <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="#">Contacto</a></li>
